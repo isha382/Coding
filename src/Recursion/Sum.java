e8695693986362c0524cd862ad1d7a5d77cac3d8
isha382 <isha.chaudhary@itinker.io>
2025-03-23 01:27:42 +0530

    parametrised and functionl recursion

--- src/Recursion/Sum.java
+++ src/Recursion/Sum.java
@@ -1,12 +1,23 @@
 package Recursion;
-
+//to find sum of n numbers
 public class Sum {
-    public static int findSum( int i,int n){
-return 0;
+    public static void findSumParametrised( int i,int sum){
+    if(i<1){
+        System.out.println(sum);
+        return;
+    }
+        findSumParametrised(i-1,sum+i);
+
+    }
+    public static int findSumFunctional(int n){
+        if(n<1){
+            return 0;
+        }
+    return   n +  findSumFunctional(n-1);
     }
 
     public static void main(String [] args){
-        int res =findSum(1,5);
-        System.out.println("sum is :::" + res);
+        findSumParametrised(5,0);
+        System.out.println(findSumFunctional(5));
     }
 }
