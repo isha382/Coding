895e3939c10ad33317591df43d4507599f2c7ec6
isha382 <isha.chaudhary@itinker.io>
2025-03-22 23:42:18 +0530

    recursion printing from n to 1

--- src/Recursion/LinearPrint.java
+++ src/Recursion/LinearPrint.java
@@ -8,8 +8,17 @@ public class LinearPrint {
         System.out.print(i+ " ");
         linearPrint(i+1,n);
     }
+    public static void reverseLinearPrint(int i,int n){
+        if(i<1){
+            return;
+        }
+        System.out.println();
+        System.out.print(i+ " ") ;
+        reverseLinearPrint(i-1,n);
+    }
     public static void main(String [] args){
         int n= 10;
         linearPrint(1,10);
+        reverseLinearPrint(10,10);
     }
 }
