99cec0634a19e0dd3f98fbee3be3a84f37fd8a09
isha382 <isha.chaudhary@itinker.io>
2025-03-23 01:02:34 +0530

    recursion printing from n to 1 and n to 1 using backtracking

--- src/Recursion/LinearPrint.java
+++ src/Recursion/LinearPrint.java
@@ -8,17 +8,40 @@ public class LinearPrint {
         System.out.print(i+ " ");
         linearPrint(i+1,n);
     }
+
+
+    public static void linearPrintBacktracking(int i,int n){
+        if(i<1){
+            return;
+        }
+        linearPrintBacktracking(i-1,n);
+        System.out.print(i+" ");
+    }
+
     public static void reverseLinearPrint(int i,int n){
+
         if(i<1){
             return;
         }
         System.out.println();
         System.out.print(i+ " ") ;
+
         reverseLinearPrint(i-1,n);
+    }
+
+    public static void reverseLinearPrintBacktraking(int i,int n){
+        //using backtracking
+        if(i>n){
+            return;
+        }
+        reverseLinearPrintBacktraking(i+1,n);
+        System.out.print(i + " ");
     }
     public static void main(String [] args){
         int n= 10;
         linearPrint(1,10);
         reverseLinearPrint(10,10);
+        linearPrintBacktracking(10,10);
+        reverseLinearPrintBacktraking(1,10);
     }
 }
--- src/Recursion/Sum.java
+++ src/Recursion/Sum.java
@@ -1,2 +1,12 @@
-package Recursion;public class Sum {
+package Recursion;
+
+public class Sum {
+    public static int findSum( int i,int n){
+return 0;
+    }
+
+    public static void main(String [] args){
+        int res =findSum(1,5);
+        System.out.println("sum is :::" + res);
+    }
 }
