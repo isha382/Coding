b1601561186eb5d0964c9a70bf39d459855c8e26
isha382 <isha.chaudhary@itinker.io>
2025-03-23 01:54:04 +0530

    factorial of a number

---
+++ src/Recursion/Factorial.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class Factorial {
+    public static int findFactorial(int num){
+        if(num <= 1){
+            return 1;
+        }
+        return num * findFactorial(num-1);
+    }
+    public static void main(String [] args){
+        System.out.println(findFactorial(5));
+    }
+}
